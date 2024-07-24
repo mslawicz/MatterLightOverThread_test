5e96d12ce6bf20f6c4e3db17f779107bc69d31c8
Marcin Slawicz <marcin.slawicz@etteplan.com>
2024-07-24 16:21:42 +0200

    my thread added

--- src/main.cpp
+++ src/main.cpp
@@ -20,12 +20,35 @@
 #include "sl_system_init.h"
 #include "sl_system_kernel.h"
 #include <MatterConfig.h>
+#include "cmsis_os2.h"
+
+constexpr uint32_t myTaskStackSize = 1024;
+// Task is dynamically allocated with max priority. This task gets deleted once the inits are completed.
+constexpr osThreadAttr_t myTaskAttr = { .name       = "my Task",
+                                        .attr_bits  = osThreadDetached,
+                                        .cb_mem     = NULL,
+                                        .cb_size    = 0U,
+                                        .stack_mem  = NULL,
+                                        .stack_size = myTaskStackSize,
+                                        .priority   = osPriorityLow };
+osThreadId_t myTaskHandle;
+
+void myApplicationStart(void * unused);
 
 int main(void)
 {
     sl_system_init();
+    myTaskHandle = osThreadNew(myApplicationStart, nullptr, &myTaskAttr);
 
     // Initialize the application. For example, create periodic timer(s) or
     // task(s) if the kernel is present.
     SilabsMatterConfig::AppInit();
+}
+
+void myApplicationStart(void * unused)
+{
+    while(1)
+    {
+        osDelay(500);
+    }
 }
